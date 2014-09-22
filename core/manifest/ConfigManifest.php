12df1f96570c83ecd1f194236807bdd23a04b450
mattclegg <cleggmatt@gmail.com>
2014-09-22 21:48:34 +0200

    Check if $fragment exists before using as array

--- core/manifest/ConfigManifest.php
+++ core/manifest/ConfigManifest.php
@@ -652,7 +652,7 @@ class SS_ConfigManifest {
 	 * @return void
 	 */
 	public function mergeInYamlFragment(&$into, $fragment) {
-		foreach ($fragment as $k => $v) {
+		if($fragment) foreach ($fragment as $k => $v) {
 			Config::merge_high_into_low($into[$k], $v);
 		}
 	}
